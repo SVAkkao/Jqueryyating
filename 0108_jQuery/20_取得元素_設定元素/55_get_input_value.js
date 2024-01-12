3e9d7f552dc203132808cd718f45c229f81b2173
Mingzhen0814 <fcum1102930@gmail.com>
2024-01-12 15:20:35 +0800

    jQuery上課練習檔案

--- 0108_jQuery/20_取得元素_設定元素/55_get_input_value.js
+++ 0108_jQuery/20_取得元素_設定元素/55_get_input_value.js
@@ -1,7 +1,14 @@
 // 10. 如果是這樣寫的話，有錯誤訊息，為什麼？
-btnGet.onclick = function(){
-    console.log("OK");
-}
+
+//1.在元素誕生(12)之前就想要存取元素(7)
+//2.script應該要放在</body>之前
+//3.放在head不對
+//3.應放在document ready 之後做
+//筆試
+
+// btnGet.onclick = function(){
+//     console.log("OK");
+// }
 
 
 
@@ -81,18 +88,58 @@ btnGet.onclick = function(){
 // 11. 畫面準備好以後才開始執行
 $(function () {
     btnGet.onclick = function () {
-
+        
         // 20. 取得 姓名欄位 (使用 JavaScript 的方式)
         // 21. 取得 姓名欄位 (使用 jQuery 的方式)
+var js1 = userName.value;
+var jq1 = $('#userName').val();
+console.log(js1);
+console.log(jq1);
 
-
+        console.clear();
         // 30. 取得 地址欄位 (使用 JavaScript 的方式)
         // 31. 取得 地址欄位 (使用 jQuery 的方式)
+        var js2 = document.getElementById('address').value
+        var jq2 = $('#address').val();
+        console.log(js2);
+        console.log(jq2);
 
-
+        console.clear()
         // 40. 取得 年紀欄位 (使用 JavaScript 的方式)
-        // 41. 取得 年紀欄位 (使用 jQuery 的方式)
+        //要如何獲得年紀的元素有哪些?       
+        //獲取使用者透過radio選擇的年紀
+        //判斷radio被選取checked
+        // if(document.getElementByName('age').checked==true)
+        // var elem = document.getElementsByName('age');
+        // console.log(elem); //陣列[] 類陣列[]
 
+        // console.log(elem[0]);
+        // console.log(elem[1]);
+        // console.log(elem[2]);
+        // console.log(elem[3]);
+
+        // console.log(elem[0].checked) // true
+        // console.log(elem[1].checked) // false
+        // console.log(elem[2].checked) // false
+        // console.log(elem[3].checked) // false
+
+        // for(let index = 0; index <elem.length; index++){
+        //     if(elem[index].checked == true){
+        //         console.log(elem[index].value);
+        //     }
+        // }
+        //document.querySelectorAll
+
+        //取得該radio的value
+
+
+        // 41. 取得 年紀欄位 (使用 jQuery 的方式)
+        //$(選擇元素).行動();
+        //CSS 要怎麼選擇 checked
+
+        console.clear();
+        var temp = $("input[name='age']:checked").val();
+        console.log(temp);
     }
 })
 
--- 0108_jQuery/20_取得元素_設定元素/56_set_input.js
+++ 0108_jQuery/20_取得元素_設定元素/56_set_input.js
@@ -2,11 +2,18 @@ $(function () {
 
     // 10. 練習設定初始值
     btnSet.onclick = function () {
-
         // 20. 將地址預設為台中
+        // $('#address').val("Banciao");
+        // $("#address").prop('value',"Taichung")
+
+        $('option[value="Taichung"]').prop("selected",true);
 
         // 30. 將職業預設為其他
 
+        $('input[value="teacher"]+input[value="other"]').prop("checked",true)
+
         // 40. 將交通工具預設為機車
+
+        $('input[name="vehicle"]input[value="motorcycle"]').prop("checked",true)
     }
 })
